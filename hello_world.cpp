17e147a5d30780cd6539299eccc1881160c24877
arinacherednik <arina.cherednik@yandex.ru>
2022-04-21 21:11:44 +0300

    clang format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
- #include <iostream>
+#include <iostream>
 
-int main()
+int
+main()
 {
-// Комментарий1
-//Комментарий2
-std::cout << "Пожалуйста, введите свое имя"<<endl;
-std::string name;
-std::cin >> name;
-std::cout<<" Hello world from "<<name<<std::endl;
+  // Комментарий1
+  //Комментарий2
+  std::cout << "Пожалуйста, введите свое имя" << endl;
+  std::string name;
+  std::cin >> name;
+  std::cout << " Hello world from " << name << std::endl;
 }
